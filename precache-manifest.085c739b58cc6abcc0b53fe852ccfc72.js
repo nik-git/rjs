9e8bd50406e8f2f4e3616d48cfc238ea72550f50
Nikhil Gupta <16205192+nik-git@users.noreply.github.com>
2020-01-30 15:49:15 +0530

    qqqqq
    qqqqqq

---
+++ precache-manifest.085c739b58cc6abcc0b53fe852ccfc72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff6862f8626af62942be8400153955b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c6fcb6bda0dc4c35417",
+    "url": "/static/js/2.34f8809a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.34f8809a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9d9447f3a2a24bd1f947",
+    "url": "/static/js/main.57a44936.chunk.js"
+  },
+  {
+    "revision": "2f02f8afdff846c3110a",
+    "url": "/static/js/runtime-main.156f9a3b.js"
+  }
+]);
